b20b9761ff1a5cbe750f176814c02161a27295b7
patilharsh006-lab <patilharsh006@gmail.com>
2026-07-16 15:11:27 +0530

    Remove app context for database creation
    Removed the context manager for database creation.

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///transport_management.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
 
 # Models
 class Vehicle(db.Model):
